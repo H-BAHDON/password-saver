367c248184c3bd0ad7c42e64b264e8c63ced2127
hussein bahdon <husseinbahdon1@gmail.com>
2023-03-06 23:41:17 +0000

    updated App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 
+import Home from "./pages/home"
+import "./App.css"
+
 
 function App() {
-  return (
-    <div >
-      <h1>Hello World</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Home />
+            
+     
+        </div>
+    );
 }
 
 export default App;
