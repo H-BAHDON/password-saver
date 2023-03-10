e0b8ac8b23a68616a083fde01dbbcce7977d84c2
hussein bahdon <husseinbahdon1@gmail.com>
2023-03-10 01:58:17 +0000

    Created Header

---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../styles/header.css"
+
+export default function header() {
+    return (
+
+        <div className="navbar">
+            <div className="navbar-title">
+                <h1>
+                    Password Saver
+                </h1>
+            </div>
+
+        </div>
+
+    )
+}
