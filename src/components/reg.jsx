0edfc994a0929dd8d78d428b4154de108f6623cb
Hussein Bahdon <35459369+Dead-Section@users.noreply.github.com>
2023-03-11 12:41:49 +0000

    Summary Updated

--- src/components/reg.jsx
+++ src/components/reg.jsx
@@ -1,40 +1,65 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+
 
 function Reg() {
 
-    const histroy = useNavigate();
+    const history = useNavigate();
 
-    const[date, setDate] = useState([])
+    const[data, setData] = useState([])
 
     const [details, setDetails] = useState({
       name: "",
       email: "",
       password: "",  
     })
-    
 
+    console.log(details);
+    
+    const handleChange = (e) => {
+        const { value, name } = e.target;
+
+        setDetails(() =>{
+          return {
+            ...details,
+            [name]: value
+          }
+        })
+    }
+
+    const handleClick = (e) => {
+        const {username, email, password} = details;
+
+        if (username && email && password === ""){
+          toast.error('name field is requred!');
+        }else{
+          console.log("data added succesfully");
+          history("/Login")
+          localStorage.setItem("Users",JSON.stringify([...data,details]));
+        }
+        e.preventDefault();
+
+    }
 
 
   return (
     <div>
     <form>  
-        <div class="container">   
+        <div className="container">   
             <label>Your Name : </label>   
-            <input type="text" placeholder="Enter Email" name="username" required/> 
+            <input type="text" placeholder="Enter Name" onChange={handleChange} name="name" value={details.name} required/> 
 
             <label>Email: </label>   
-            <input type="email" placeholder="Enter Username" name="email" required/> 
+            <input type="email" placeholder="Enter Email" onChange={handleChange} name="email" value={details.email} required/> 
 
             <label>Password : </label>   
-            <input type="password" placeholder="Enter Password" name="password" required/>  
+            <input type="password" placeholder="Enter Password" onChange={handleChange} name="password" value={details.password} required/>  
             
-            <button type="submit">Login</button>  
-            <p className='mt-3'>Already Have an Account <span><NavLink to="/login">SignIn</NavLink></span> </p> 
+            <button type="submit" onClick={handleClick}>Login</button>  
         </div>   
      </form>  
-      
     </div>
   )
 }
