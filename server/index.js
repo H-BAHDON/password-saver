fa3a01692df7c82313fb1d6c64d7048791322f05
hussein bahdon <husseinbahdon1@gmail.com>
2023-03-23 02:22:08 +0000

    updated index.js

--- server/index.js
+++ server/index.js
@@ -1,18 +1,40 @@
-const express = require("express");
-const mysql = require("mysql");
-const cors = require("cors");
+
+// ----------------------------------------
+    const express = require("express");
+    // const bodyParser = require("body-parser");
+    // const cookieParser = require("cookie-parser");
+
+// ----------------------------------------
+// ----------------------------------------
+//        Importing Middleware from auth folder
+// const { corsMiddleware, sessionMiddleware } = require("./auth/authMiddleware");
+
+
+  /// leaving a note to add the build folder from src that you 
+  // will create into the server folder: well done Hussein Bahdon
+
 
 const app = express();
 
-app.use(express.json());
+//----------------------------------------
+//      cookie & bodyParser
+// app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: true }));
+//----------------------------------------
 
-const db = mysql.createConnection({
-    host: 'sql8.freemysqlhosting.net',
-    user: 'sql8605460',
-    password: 'Holloway12!"',
-    database: 'Users',
-  });
+// app.use(express.json());
+// app.use(sessionMiddleware);
+// app.use(corsMiddleware);
+//----------------------------------------
 
-app.listen(3001, () => {
+// const loginRoute = require('./login');
+// const registerRoute = require('./routes/register')
+
+// registerRoute(app)
+// loginRoute(app)
+
+//----------------------------------------
+app.listen(4001, () => {
   console.log("running server");
 });
+
