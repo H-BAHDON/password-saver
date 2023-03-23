42701f3851bf3f205352769a91e5676c826a5968
hussein bahdon <husseinbahdon1@gmail.com>
2023-03-23 02:21:52 +0000

    Created db.js
    connected

---
+++ server/database/db.js
@@ -0,0 +1,18 @@
+const { Pool } = require("pg");
+
+const db = new Pool({
+  user: "fessvndx",
+  host: "surus.db.elephantsql.com",
+  database: "fessvndx",
+  password: "nb3-MIo5Nh9pfE3n1j9GlSgvecUtrG2v",
+  port: 5432,
+});
+
+db.connect((err, client, release) => {
+  if (err) {
+    return console.error("Error acquiring client", err.stack);
+  }
+  console.log("Connected to database");
+});
+
+module.exports = db;
