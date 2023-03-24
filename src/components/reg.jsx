817f3b383371206e923fded6284a19cb77c3c53c
hussein bahdon <husseinbahdon1@gmail.com>
2023-03-24 12:58:50 +0000

    Updated Reg.jsx

--- src/components/reg.jsx
+++ src/components/reg.jsx
@@ -1,88 +1,76 @@
-import React, { useEffect, useState } from "react";
+import React, {useState} from "react";
 import Axios from "axios";
 
 export default function Registration() {
-  const [usernameReg, setUsernameReg] = useState("");
-  const [passwordReg, setPasswordReg] = useState("");
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const [loginStatus, setLoginStatus] = useState("");
+  const {registrationUser, setRegistrationUser} = useState({
+    firstName: "",
+    secondName: "",
+    email: "",
+    password: "",
+  });
 
   Axios.defaults.withCredentials = true;
 
   const register = () => {
-    Axios.post("http://localhost:3001/register", {
-      username: usernameReg,
-      password: passwordReg,
+    Axios.post("http://localhost:4001/register", {
+      firstName: registrationUser.firstName,
+      secondName: registrationUser.secondName,
+      email: registrationUser.email,
+      password: registrationUser.password,
     }).then((response) => {
       console.log(response);
     });
   };
 
-  const login = () => {
-    Axios.post("http://localhost:3001/login", {
-      username: username,
-      password: password,
-    }).then((response) => {
-      if (response.data.message) {
-        setLoginStatus(response.data.message);
-      } else {
-        setLoginStatus(response.data[0].username);
-      }
+  const handleChange = (event) => {
+    setRegistrationUser({
+      ...registrationUser,
+      [event.target.name]: event.target.value,
     });
   };
 
-  useEffect(() => {
-    Axios.get("http://localhost:3001/login").then((response) => {
-      if (response.data.loggedIn == true) {
-        setLoginStatus(response.data.user[0].username);
-      }
-    });
-  }, []);
-
   return (
-    <div className="App">
-      <div className="registration">
-        <h1>Registration</h1>
-        <label>Username</label>
+    <div id="registration">
+      <h2>Registration</h2>
+      <form>
+        <label htmlFor="firstName">First Name:</label>
         <input
           type="text"
-          onChange={(e) => {
-            setUsernameReg(e.target.value);
-          }}
+          id="firstName"
+          name="firstName"
+          onChange={handleChange}
+          required
         />
-        <label>Password</label>
-        <input
-          type="text"
-          onChange={(e) => {
-            setPasswordReg(e.target.value);
-          }}
-        />
-        <button onClick={register}> Register </button>
-      </div>
 
-      <div className="login">
-        <h1>Login</h1>
+        <label htmlFor="secondName">Second Name:</label>
         <input
           type="text"
-          placeholder="Username..."
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
+          id="secondName"
+          name="secondName"
+          onChange={handleChange}
+          required
         />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="password">Password:</label>
         <input
           type="password"
-          placeholder="Password..."
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
+          id="password"
+          name="password"
+          onChange={handleChange}
+          required
         />
-        <button onClick={login}> Login </button>
-      </div>
 
-      <h1>{loginStatus}</h1>
+        <input type="submit" onClick={register} value="Register" />
+      </form>
     </div>
   );
 }
